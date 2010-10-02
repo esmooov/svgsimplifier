bafe3287249ce56e77d834eb76eb25a15030fa51
Erik Hinton <twotonmary@gmail.com>
2010-10-02 14:35:08 -0400

    Set up reader/parser

--- src/svg.rb
+++ src/svg.rb
@@ -1,5 +1,29 @@
 # Ruby internal object representation of an SVG diagram
 
-module SVG
+require 'rubygems'
+require 'crack'
+
+class SVG
+
+	def initialize(source) 
+		
+		str = ""
+		begin
+			File.open(source,"r") do |f|
+				str = f.read
+			end
+		rescue
+			str = source
+		end
+
+		@svg = Crack::XML.parse(str)
+
+	end
+
+	def to_s
+		
+		@svg.inspect
+
+	end
 
 end
