7de85016a6784ba38388f4de95975f5ff22983b7
Erik Hinton <twotonmary@gmail.com>
2010-10-03 18:51:51 -0400

    Got juan's new svg

--- src/svg_point.rb
+++ src/svg_point.rb
@@ -4,14 +4,12 @@ require 'point'
 
 class SVGPoint
 
-	def initialize(type, p = Point.new(nil, nil), r1 = Point.new(nil, nil), r2 = Point.new(nil, nil), keep = true)
-		
+	def initialize(type, p = Point.new(nil, nil), r1 = Point.new(nil, nil), r2 = Point.new(nil, nil), keep = false)		
 		@type = type
 		@p = p
 		@r1 = r1
 		@r2 = r2
 		@keep = keep
-
 	end
 
 	def to_s
